ed301f5228749c36b4b2a0cfeefe0da9c2d9a81c
MdMahadiHasan11 <hasanpustcse11@gmail.com>
2025-01-21 19:22:42 +0600

    done

--- index.js
+++ index.js
@@ -14,9 +14,10 @@ const port = process.env.PORT || 5000;
 
 // // middleware
 app.use(cors({
-    origin: true, // Allows all origins
-    credentials: true // Allows credentials (cookies, Authorization headers, etc.)
+    origin: '*', // Allow all origins
+    credentials: true
 }));
+app.use(express.json());
 
 
 // const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vdildbx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
